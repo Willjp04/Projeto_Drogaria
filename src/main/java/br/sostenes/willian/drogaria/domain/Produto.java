f45a9adbcd81036d8e59e863359aae8bd4edf3f6
Willian Sostenes <sostenes655@gmail.com>
2022-06-20 03:51:37 -0300

    Inserindo parametros e configs para os atributos da classe Produto para ser criado no banco de dados Mysql

--- src/main/java/br/sostenes/willian/drogaria/domain/Produto.java
+++ src/main/java/br/sostenes/willian/drogaria/domain/Produto.java
@@ -3,26 +3,48 @@ package br.sostenes.willian.drogaria.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 //PODEMOS UTILIZAR AS ANOTAÇÕES SEPARADAS TAMBÉM
 
 // ABAIXO TODAS AS ANOTAÇÕES SÃO DO LOMBOK
 
-@Data //ESTA ANOTAÇÃO DATA É DO LOMBOK QUE CRIA OS MÉTODOS CONSTRUTOR, GETTERS, SETTERS, HASH, EQUALS.
+@Data // ESTA ANOTAÇÃO DATA É DO LOMBOK QUE CRIA OS MÉTODOS CONSTRUTOR, GETTERS,
+		// SETTERS, HASH, EQUALS.
 @NoArgsConstructor // ESTA ANOTAÇÃO É PARA UTILIZAR O CONSTRUTOR PADRÃO
 @AllArgsConstructor // ESTA ANOTAÇÃO É PARA UTILIZAR O CONSTRUTOR COM TODOS OS ATRIBUTOS.
+@Entity // ESTA ANOTAÇÃO INFORMA QUE ESSA CLASSE É UMA TABELA NO BANCO DE DADOS
 public class Produto {
 
-	
+	@Id // ESTA ANOTAÇÃO SERVE PARA INFORMAR QUE ESTE ATRIBUTO É A CHAVE PRIMÁRIA
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // ESTA ANOTAÇÃO VAI ATRIBUIR UMA CHAVE PRIMÁRIA AUTOMÁTICA PARA O ATRIBUTO
 	private Integer codigo;
-	private String nome;
-	private Integer quantidade;
-	private BigDecimal preco;
 
+	
+	@Column(length = 50, nullable = false,unique = true) // ESTA ANOTAÇÃO SERVE PARA INDICAR QUE ESTE ATRIBUTO SERÁ UMA COLUNA 
+	//O LENGTH É PARA INDICAR O TAMANHO MÁXIMO DE CARACTERES
+	//O NULLABLE COMO FALSE INDICA QUE ESTE ATRIBUTO É DE PREENCHIMENTO OBRIGATÓRIO
+	//O UNIQUE COMO TRUE É PARA INDICAR QUE O NOME NÃO PODE SE REPETIR
+	private String nome;
+	
+	@Column(nullable = false)
+	private Integer quantidade;
+	
+	@Column(nullable = false, precision = 5,scale = 2)
+		//O PRECISION INDICA A QUANTIDADE DE NÚMEROS TOTAIS
+	//O SCALE INDICA A QUANTIDADE DE NÚMEROS TOTAIS DEPOIS DA VIRGULA
+	// NO CASO ACIMA FICARÁ NO MÁXIMO DE 999,99 REAIS
+	private BigDecimal preco;
+	
+	@Column
 	private LocalDate dataDeValidade;
 
 }
