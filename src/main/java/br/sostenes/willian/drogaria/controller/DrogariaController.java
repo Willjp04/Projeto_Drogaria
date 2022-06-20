684e8e85a4b76bdf414226cc8ca47e7685e13354
Willian Sostenes <sostenes655@gmail.com>
2022-06-20 02:52:13 -0300

    Instalando o jpa data e configurando a conexão com o Banco de dados

--- src/main/java/br/sostenes/willian/drogaria/controller/DrogariaController.java
+++ src/main/java/br/sostenes/willian/drogaria/controller/DrogariaController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 //ESTA É MINHA CLASSE PARA CONTROLE
 
-
 // A ANOTAÇÃO RESTCONTROLLER É PARA INFORMAR QUE ESSA CLASSE É UM CONTROLADOR
 //ENTRE PARENTESES EU VOU COLOCAR O CAMINHO QUANOD EU QUISER CHAMAR ESTE CONTROLADOR
 @RestController
@@ -16,7 +15,7 @@ public class DrogariaController {
 	// E TAMBÉM PRECISA DE UMA ANOTAÇÃO
 	@RequestMapping("/ola")
 	public String retornaMensagem() {
-		return "Olá Drogaria";
+		return "Olá drogaria";
 	}
 
 }
