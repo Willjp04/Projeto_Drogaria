4c1aca6d53dc7fc120b141f9c13afa5488f49f80
Willian Sostenes <sostenes655@gmail.com>
2022-06-20 02:14:19 -0300

    Iniciando projeto

---
+++ src/test/java/br/sostenes/willian/drogaria/DrogariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sostenes.willian.drogaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrogariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
