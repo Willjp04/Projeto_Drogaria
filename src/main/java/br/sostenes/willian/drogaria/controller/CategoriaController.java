7d12ff28e7d5074ed56c33791142ac2d9d652d92
Willian Sostenes <sostenes655@gmail.com>
2022-06-20 07:00:22 -0300

    Criação do método deletar na classe controller de Categorias

--- src/main/java/br/sostenes/willian/drogaria/controller/CategoriaController.java
+++ src/main/java/br/sostenes/willian/drogaria/controller/CategoriaController.java
@@ -1,9 +1,12 @@
 package br.sostenes.willian.drogaria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,25 +20,30 @@ import br.sostenes.willian.drogaria.repository.CategoriaRepository;
 public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
-	
-	//ESTA LISTA É PARA RETORNAR TODAS AS CATEGORIAS 
+
+	// ESTA LISTA É PARA RETORNAR TODAS AS CATEGORIAS
 	@GetMapping("/")
-	public List<Categoria> listar(){
-		List<Categoria> categorias = categoriaRepository.findAll(); 
+	public List<Categoria> listar() {
+		List<Categoria> categorias = categoriaRepository.findAll();
 		return categorias;
-		
+
 	}
-	
-	//ESTE MÉTODO É PARA INSERÇÃO
-	@PostMapping //ESTA ANOTAÇÃO É SEMPRE PARA INSERÇÃO DE DADOS
-	
-	public Categoria inserir(@RequestBody Categoria categoria) { //ESTA ANOTAÇÃO É PARA MAPEAR ESTE OBJETO
-	Categoria categoriaSalva = categoriaRepository.save(categoria);
-	return categoriaSalva;
-		
-	
-		
+
+	// ESTE MÉTODO É PARA INSERÇÃO
+	@PostMapping // ESTA ANOTAÇÃO É SEMPRE PARA INSERÇÃO DE DADOS
+
+	public Categoria inserir(@RequestBody Categoria categoria) { // ESTA ANOTAÇÃO É PARA MAPEAR ESTE OBJETO
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		return categoriaSalva;
+
+	}
+
+	@DeleteMapping("/{codigo}")
+	public Categoria deletar(@PathVariable Integer codigo) {
+		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		categoriaRepository.delete(categoria.get());
+		return categoria.get();
+
 	}
 
 }
