bd06d012644b05229851f15d2db152abae01ee73
Willian Sostenes <sostenes655@gmail.com>
2022-06-20 07:27:27 -0300

    Criação do método buscar na classe controller de Categorias, para buscar a categoria separadamente

--- src/main/java/br/sostenes/willian/drogaria/controller/CategoriaController.java
+++ src/main/java/br/sostenes/willian/drogaria/controller/CategoriaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,8 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
-	// ESTA LISTA É PARA RETORNAR TODAS AS CATEGORIAS
+	
+	// ESTE MÉTODO É PARA RETORNAR TODAS AS CATEGORIAS, FOI CRIADO UMA LISTA PASSANDO CATEGORIAS DENTRO
 	@GetMapping("/")
 	public List<Categoria> listar() {
 		List<Categoria> categorias = categoriaRepository.findAll();
@@ -45,6 +47,23 @@ public class CategoriaController {
 		categoriaRepository.delete(categoria.get());
 		return categoria.get();
 
+	}
+	//ESTE MÉTODO É PARA EDIÇÃO 
+	@PutMapping 
+	public Categoria editar(@RequestBody Categoria categoria) {
+		Categoria categoriaEditada = categoriaRepository.save(categoria);
+		return categoriaEditada;
+		
+			
+	}
+	
+	// ESTE MÉTODO É PARA BUSCAR UMA CATEGORIA POR VEZ
+	@GetMapping("/{codigo}")
+		public Categoria buscar(@PathVariable Integer codigo) {
+		Optional<Categoria> resultado = categoriaRepository.findById(codigo);
+		Categoria categoria = resultado.get();
+		return categoria;
+		
 	}
 
 }
