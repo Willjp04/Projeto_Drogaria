1ecfc563ee3094ad5bdb8868ca8803f5c9372173
Willian Sostenes <sostenes655@gmail.com>
2022-06-20 06:37:35 -0300

    Criação do metodo inserir na classe Controller de Categorias

--- src/main/java/br/sostenes/willian/drogaria/controller/CategoriaController.java
+++ src/main/java/br/sostenes/willian/drogaria/controller/CategoriaController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,17 @@ public class CategoriaController {
 	public List<Categoria> listar(){
 		List<Categoria> categorias = categoriaRepository.findAll(); 
 		return categorias;
+		
+	}
+	
+	//ESTE MÉTODO É PARA INSERÇÃO
+	@PostMapping //ESTA ANOTAÇÃO É SEMPRE PARA INSERÇÃO DE DADOS
+	
+	public Categoria inserir(@RequestBody Categoria categoria) { //ESTA ANOTAÇÃO É PARA MAPEAR ESTE OBJETO
+	Categoria categoriaSalva = categoriaRepository.save(categoria);
+	return categoriaSalva;
+		
+	
 		
 	}
 
